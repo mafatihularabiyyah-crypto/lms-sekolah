99ce87de32f1b624f875c1327f25ed33b9a7f4a4
TarbiyahTech <mafatihularabiyyah@gmail.com>
2026-06-14 09:03:09 +0700

    v10

--- app/(akun)/admin/users/create/actions.ts
+++ app/(akun)/admin/users/create/actions.ts
@@ -1,11 +1,9 @@
-// app/(dashboard)/admin/students/create/actions.ts
 "use server";
 
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import bcrypt from "bcrypt";
-import { Gender } from "@prisma/client";
 
 export async function createStudent(formData: FormData) {
   // 1. Ambil data Akun Login (User)
@@ -15,9 +13,6 @@ export async function createStudent(formData: FormData) {
   
   // 2. Ambil data Profil Akademik
   const nis = formData.get("nis") as string;
-  const gender = formData.get("gender") as Gender;
-  const birthPlace = formData.get("birthPlace") as string;
-  const birthDate = formData.get("birthDate") as string; // Format: YYYY-MM-DD
   const parentName = formData.get("parentName") as string;
   const parentPhone = formData.get("parentPhone") as string;
   const enrollYear = parseInt(formData.get("enrollYear") as string);
@@ -34,7 +29,7 @@ export async function createStudent(formData: FormData) {
 
   try {
     // TRANSAKSI DATABASE: Simpan Akun dan Profil secara bersamaan!
-    await prisma.$transaction(async (tx) => {
+    await prisma.$transaction(async (tx: any) => {
       // Step A: Buat akun otentikasi
       const newUser = await tx.user.create({
         data: {
@@ -50,9 +45,6 @@ export async function createStudent(formData: FormData) {
       await tx.studentProfile.create({
         data: {
           nis,
-          gender,
-          birthPlace: birthPlace || null,
-          birthDate: birthDate ? new Date(birthDate) : null,
           parentName: parentName || null,
           parentPhone: parentPhone || null,
           enrollYear,
@@ -63,7 +55,7 @@ export async function createStudent(formData: FormData) {
     });
   } catch (error) {
     console.error("Gagal menyimpan data:", error);
-    throw new Error("Gagal menyimpan data santri. Pastikan NIS belum digunakan.");
+    throw new Error("Gagal menyimpan data santri. Pastikan NIS atau Email belum digunakan.");
   }
 
   // Jika sukses, refresh tabel dan kembali ke direktori
